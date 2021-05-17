bb2ba596c7242eaa4ce2bb57b7aa5a11fba29758
PriyaGondaliya <priyagondaliya444@gmail.com>
2021-05-17 00:21:33 -0400

    Recipe page

---
+++ Js/script.js
@@ -0,0 +1,257 @@
+let breakfastItem = [
+    {
+        'id': 0,
+        'name': 'Red Jersey',
+        'description': 'Manchester United Home Jersey, red, sponsored by Chevrolet',
+        'image':'pic1.jpeg',
+        'ingredients':' ½ pound (225 grams) fried small pasta shells, regular, whole wheat, or if you’re a poor planner, a mix<br>1 cup (455 grams) fresh shelled peas (from about 1 pound in the pod)<br>1 cup (240 ml) heavy cream<br>3 tbsp (40 grams) unsalted butter<br>Freshly ground black pepper<br>1 tsp finely grated fresh lemon zest<br>1 cup (115 grams) finely grated Parmesan cheese<br>2 tbsp chopped fresh flat-leaf parsley',
+        'instruction1':' Bring a large pot of salted water to boil. Add the pasta, and cooked according to package instructions. Add peas to cook during the last 30 seconds of pasta cooking time. Reserve ½ cup pasta cooking water, and set aside. Drain the pasta and the peas together.',
+        'instruction2':' Dry out the pasta pot, and pour in the heavy cream. Bring the cram to a simmer, and cook it until slightly reduced, about 4 minutes, stirring frequently. Add the butter, and stir it until it has melted. Generously season the sauce with freshly ground black pepper; add a pinch of salt as well as the lemon zest. Add ¾ cup of Parmesan, and stir it until the sauce is smooth; then toss in the drained pasta and pas. Cook the pasta in sauce for 2 minutes, until the sauce has slightly thickened. Add the reserved pasta water by the spoonful if needed to loosen the sauce.',
+        'instruction3':'Divide the pasta among bowls. Garnish with remaining Parmesan and the flat-leaf parsley'        
+    },
+    {
+        'id': 1,
+        'name': 'White Jersey',
+        'description': 'Manchester United Away Jersey, white, sponsored by Chevrolet',
+        'image':'pic2.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+       
+
+    },
+    {
+        'id': 2,
+        'name': 'Black Jersey',
+        'description': 'Manchester United Extra Jersey, black, sponsored by Chevrolet',
+        'image':'pic3.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+       
+    },
+    {
+        'id': 3,
+        'name': 'Blue Jacket',
+        'description': 'Blue Jacket for cold and raniy weather',
+        'image':'pic4.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+       
+    },
+    {
+        'id': 4,
+        'name': 'Snapback Cap',
+        'description': 'Manchester United New Era Snapback Cap- Adult',
+        'image':'pic5.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+        
+    },
+    {
+        'id': 5,
+        'name': 'Champion Flag',
+        'description': 'Manchester United Champions League Flag',
+        'image':'pic6.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+        
+    }
+]
+let lunchItem = [
+    {
+        'id': 0,
+        'name': 'Red Jersey',
+        'description': 'Manchester United Home Jersey, red, sponsored by Chevrolet',
+        'image':'pic1.jpeg',
+        'ingredients':' ½ pound (225 grams) fried small pasta shells, regular, whole wheat, or if you’re a poor planner, a mix<br>1 cup (455 grams) fresh shelled peas (from about 1 pound in the pod)<br>1 cup (240 ml) heavy cream<br>3 tbsp (40 grams) unsalted butter<br>Freshly ground black pepper<br>1 tsp finely grated fresh lemon zest<br>1 cup (115 grams) finely grated Parmesan cheese<br>2 tbsp chopped fresh flat-leaf parsley',
+        'instruction1':' Bring a large pot of salted water to boil. Add the pasta, and cooked according to package instructions. Add peas to cook during the last 30 seconds of pasta cooking time. Reserve ½ cup pasta cooking water, and set aside. Drain the pasta and the peas together.',
+        'instruction2':' Dry out the pasta pot, and pour in the heavy cream. Bring the cram to a simmer, and cook it until slightly reduced, about 4 minutes, stirring frequently. Add the butter, and stir it until it has melted. Generously season the sauce with freshly ground black pepper; add a pinch of salt as well as the lemon zest. Add ¾ cup of Parmesan, and stir it until the sauce is smooth; then toss in the drained pasta and pas. Cook the pasta in sauce for 2 minutes, until the sauce has slightly thickened. Add the reserved pasta water by the spoonful if needed to loosen the sauce.',
+        'instruction3':'Divide the pasta among bowls. Garnish with remaining Parmesan and the flat-leaf parsley'
+    },
+    {
+        'id': 1,
+        'name': 'White Jersey',
+        'description': 'Manchester United Away Jersey, white, sponsored by Chevrolet',
+        'image':'pic2.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+
+    },
+    {
+        'id': 2,
+        'name': 'Black Jersey',
+        'description': 'Manchester United Extra Jersey, black, sponsored by Chevrolet',
+        'image':'pic3.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+    },
+    {
+        'id': 3,
+        'name': 'Blue Jacket',
+        'description': 'Blue Jacket for cold and raniy weather',
+        'image':'pic4.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+    },
+    {
+        'id': 4,
+        'name': 'Snapback Cap',
+        'description': 'Manchester United New Era Snapback Cap- Adult',
+        'image':'pic5.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+    },
+    {
+        'id': 5,
+        'name': 'Champion Flag',
+        'description': 'Manchester United Champions League Flag',
+        'image':'pic6.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+    }
+]
+let dinnerItem = [
+    {
+        'id': 0,
+        'name': 'Red Jersey',
+        'description': 'Manchester United Home Jersey, red, sponsored by Chevrolet',
+        'image':'pic1.jpeg',
+        'ingredients':' ½ pound (225 grams) fried small pasta shells, regular, whole wheat, or if you’re a poor planner, a mix<br>1 cup (455 grams) fresh shelled peas (from about 1 pound in the pod)<br>1 cup (240 ml) heavy cream<br>3 tbsp (40 grams) unsalted butter<br>Freshly ground black pepper<br>1 tsp finely grated fresh lemon zest<br>1 cup (115 grams) finely grated Parmesan cheese<br>2 tbsp chopped fresh flat-leaf parsley',
+        'instruction1':' Bring a large pot of salted water to boil. Add the pasta, and cooked according to package instructions. Add peas to cook during the last 30 seconds of pasta cooking time. Reserve ½ cup pasta cooking water, and set aside. Drain the pasta and the peas together.',
+        'instruction2':' Dry out the pasta pot, and pour in the heavy cream. Bring the cram to a simmer, and cook it until slightly reduced, about 4 minutes, stirring frequently. Add the butter, and stir it until it has melted. Generously season the sauce with freshly ground black pepper; add a pinch of salt as well as the lemon zest. Add ¾ cup of Parmesan, and stir it until the sauce is smooth; then toss in the drained pasta and pas. Cook the pasta in sauce for 2 minutes, until the sauce has slightly thickened. Add the reserved pasta water by the spoonful if needed to loosen the sauce.',
+        'instruction3':'Divide the pasta among bowls. Garnish with remaining Parmesan and the flat-leaf parsley'
+    },
+    {
+        'id': 1,
+        'name': 'White Jersey',
+        'description': 'Manchester United Away Jersey, white, sponsored by Chevrolet',
+        'image':'pic2.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+
+    },
+    {
+        'id': 2,
+        'name': 'Black Jersey',
+        'description': 'Manchester United Extra Jersey, black, sponsored by Chevrolet',
+        'image':'pic3.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+    },
+    {
+        'id': 3,
+        'name': 'Blue Jacket',
+        'description': 'Blue Jacket for cold and raniy weather',
+        'image':'pic4.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+    },
+    {
+        'id': 4,
+        'name': 'Snapback Cap',
+        'description': 'Manchester United New Era Snapback Cap- Adult',
+        'image':'pic5.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+    },
+    {
+        'id': 5,
+        'name': 'Champion Flag',
+        'description': 'Manchester United Champions League Flag',
+        'image':'pic6.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+    }
+]
+let dessertItem = [
+    {
+        'id': 0,
+        'name': 'Red Jersey',
+        'description': 'Manchester United Home Jersey, red, sponsored by Chevrolet',
+        'image':'pic1.jpeg',
+        'ingredients':' ½ pound (225 grams) fried small pasta shells, regular, whole wheat, or if you’re a poor planner, a mix<br>1 cup (455 grams) fresh shelled peas (from about 1 pound in the pod)<br>1 cup (240 ml) heavy cream<br>3 tbsp (40 grams) unsalted butter<br>Freshly ground black pepper<br>1 tsp finely grated fresh lemon zest<br>1 cup (115 grams) finely grated Parmesan cheese<br>2 tbsp chopped fresh flat-leaf parsley',
+        'instruction1':' Bring a large pot of salted water to boil. Add the pasta, and cooked according to package instructions. Add peas to cook during the last 30 seconds of pasta cooking time. Reserve ½ cup pasta cooking water, and set aside. Drain the pasta and the peas together.',
+        'instruction2':' Dry out the pasta pot, and pour in the heavy cream. Bring the cram to a simmer, and cook it until slightly reduced, about 4 minutes, stirring frequently. Add the butter, and stir it until it has melted. Generously season the sauce with freshly ground black pepper; add a pinch of salt as well as the lemon zest. Add ¾ cup of Parmesan, and stir it until the sauce is smooth; then toss in the drained pasta and pas. Cook the pasta in sauce for 2 minutes, until the sauce has slightly thickened. Add the reserved pasta water by the spoonful if needed to loosen the sauce.',
+        'instruction3':'Divide the pasta among bowls. Garnish with remaining Parmesan and the flat-leaf parsley'
+    },
+    {
+        'id': 1,
+        'name': 'White Jersey',
+        'description': 'Manchester United Away Jersey, white, sponsored by Chevrolet',
+        'image':'pic2.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+
+    },
+    {
+        'id': 2,
+        'name': 'Black Jersey',
+        'description': 'Manchester United Extra Jersey, black, sponsored by Chevrolet',
+        'image':'pic3.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+    },
+    {
+        'id': 3,
+        'name': 'Blue Jacket',
+        'description': 'Blue Jacket for cold and raniy weather',
+        'image':'pic4.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+    },
+    {
+        'id': 4,
+        'name': 'Snapback Cap',
+        'description': 'Manchester United New Era Snapback Cap- Adult',
+        'image':'pic5.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+    },
+    {
+        'id': 5,
+        'name': 'Champion Flag',
+        'description': 'Manchester United Champions League Flag',
+        'image':'pic6.jpg',
+        'ingredients':'hi<br>hiiii<br>hjhjhjghgv',
+        'instruction1':'instruction1',
+        'instruction2':'instruction2',
+        'instruction3':'instruction3'
+    }
+]
